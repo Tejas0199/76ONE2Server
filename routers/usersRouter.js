fd96bf3572f1302d3b5e3fe206a9d69b0a6532fe
unknown <tejasengi01@gmail.com>
2023-02-15 18:07:16 +0530

    created patch api for users

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -2,19 +2,21 @@ const express = require('express');
 const userModel = require('../model/userModel')
 const userRouter = express.Router();
 
-userRouter.get('/',(req,res) => {
-    userModel.find({},(err,users) => {
-        if(err) {
-            res.status(500).send({ msg : "something went wrong please try again later"});
-        } else {
-            res.status(200).send({users});
-        }
-    })
+userRouter.get('/', async (req,res) => {
+    setTimeout(() => {
+        userModel.find({},(err,users) => {
+            if(err) {
+                res.status(500).send({ msg : "something went wrong please try again later"});
+            } else {
+                res.status(200).send({users});
+            }
+        })
+    },8000)
 });
 
-userRouter.post('/',(req,res) => {
+userRouter.post('/',async (req,res) => {
     const userData = req.body;
-    userModel.create(userData,(err,data) => {
+    userModel.create(userData, (err,data) => {
         if(err) {
             res.status(400).send({ msg : "Not able to create account with give cridentials"});
         } else {
@@ -27,8 +29,17 @@ userRouter.put('/',() => {
 
 })
 
-userRouter.patch('/',() => {
-
+userRouter.patch('/', async (req,res) => {
+    let userData = req.body;
+    let id = userData._id;
+    delete userData._id;
+    userModel.findByIdAndUpdate({ _id : id },{...userData},(err,data) => {
+        if(err) {
+            res.status(500).send({ msg : "something went worng please try again later"})
+        } else {
+            res.status(200).send({ msg : `${data.fname + data.lname} data as been updated successfully`});
+        }
+    })
 })
 
 userRouter.delete('/',() => {
